f139d1dee0a1f0c6a09cac0131ceb9b9c8d1967e
QuocAnh-dev <quocanh.dev404@gmail.com>
2024-12-20 09:20:00 +0700

    Done encrypt password and login process.

--- src/main/java/com/motuen/identity_service/dto/request/ErrorCode.java
+++ src/main/java/com/motuen/identity_service/dto/request/ErrorCode.java
@@ -2,6 +2,7 @@ package com.motuen.identity_service.dto.request;
 
 public enum ErrorCode {
     USER_EXISTED(1002, "User existed."),
+    USER_NOT_EXISTED(1001, "User not existed."),
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized Error."),
     USERNAME_INVALID(1003, "User name must be at least 5 characters."),
     PASSWORD_INVALID(1004, "Password must be at least 8 characters."),
---
+++ src/main/java/com/motuen/identity_service/service/AuthenticationService.java
@@ -0,0 +1,25 @@
+package com.motuen.identity_service.service;
+
+import com.motuen.identity_service.dto.request.AuthenticationRequest;
+import com.motuen.identity_service.dto.request.ErrorCode;
+import com.motuen.identity_service.exception.AppException;
+import com.motuen.identity_service.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationService {
+    UserRepository userRepository;
+    public boolean authenticate(AuthenticationRequest request){
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+    }
+}
